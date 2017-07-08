2f1ab75ed5f6739e092fe25386a3b8750f5a1822
Adam Pruner <adampruner99@gmail.com>
2017-07-08 15:04:22 -0700

    removed irrelevant stuff

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter from './Counter';
-import Navbar from './Components/Navbar';
 
 document.addEventListener('DOMContentLoaded', function() {
   ReactDOM.render(
-    React.createElement(NavBar),
-    //React.createElement(Counter),
+    React.createElement(Counter),
     document.getElementById('mount')
   );
 });
